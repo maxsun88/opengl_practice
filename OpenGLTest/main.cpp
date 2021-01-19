6cc9bd8d88e429ecda1b496fbfeb8a9986392636
MaxSwen <pls6666@126.com>
2021-01-19 17:16:07 -0500

    Initial Commit

---
+++ OpenGLTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGLTest
+//
+//  Created by Max Sun on 1/19/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
